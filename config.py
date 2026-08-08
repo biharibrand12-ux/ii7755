c48df0ed5f135170cc5662197ebcac5c1d94e21d
biharibrand12-ux <biharibrand12367@gmail.com>
2026-08-08 21:39:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,15 @@
 import os
 from os import getenv
 
-
 # ------------------------------------------------
 API_ID = int(os.environ.get("API_ID", "34422904"))
 # ------------------------------------------------
-API_HASH = os.environ.get("API_HASH","7e0002469784f47fc08a6b3d93d7ebed")
+API_HASH = os.environ.get("API_HASH", "7e0002469784f47fc08a6b3d93d7ebed")
 # ------------------------------------------------
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "8855444671:AAG9dtteX7SbrIacq4evR9ThjOL0SJfYGuo")
 # ------------------------------------------------
 BOT_USERNAME = os.environ.get("BOT_USERNAME", "ExtractByParth1_bot")
-BOT_TEXT = ":ðˆð“'ð¬ð†ðŽð‹ð”.â„¢Â®:"
+BOT_TEXT = "RADHE"
 # ------------------------------------------------
 OWNER_ID = int(os.environ.get("OWNER_ID", "5349573682"))
 # ------------------------------------------------
@@ -24,27 +23,12 @@ MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://adarshppandey937:uIoPcln9
 # -----------------------------------------------
 PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1003422914923"))
 # -----------------------------------------------
-join = '<a href="">âœ³ï¸ JOIN BACKUP</a>'
+join = '<a href="">✳️ JOIN BACKUP</a>'
 # -----------------------------------------------
 UNSPLASH_ACCESS_KEY = 'RabDRmuXXBobanmwwbvpP5LwoG4J8ox34y5Sstz-9jk'
 # -----------------------------------------------
 UNSPLASH_QUERY = 'animal baby'
 # -----------------------------------------------
-ADMIN_BOT_USERNAME = "Helpbyparth_bot" #without @
+ADMIN_BOT_USERNAME = "Helpbyparth_bot"  # without @
 
 THUMB_URL = os.environ.get("THUMB_URL", "https://ibb.co/9Snm7Ds")
-
-
-
-
-# # Bot configuration
-# API_ID = int(os.environ.get("API_ID", "22746239"))
-# API_HASH = os.environ.get("API_HASH", "a98ec8cfd8572a3a7c936cf828fe6215")
-# BOT_TOKEN = os.environ.get("BOT_TOKEN", "7547829346:AAGyfvOu47EciNhC7NUGSDEDFuBaetYYusw")
-# BOT_USERNAME = os.environ.get("BOT_USERNAME", "MassRPBot")
-# OWNER_ID = int(os.environ.get("OWNER_ID", "7463601722"))
-# SUDO_USERS = list(map(int, getenv("SUDO_USERS", "7463601722").split()))
-# CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002601604234"))
-# MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://wadiro6523:08AwfhhKRdQaS1i6@cluster0.krzxuop.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-# PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1002601604234"))
-# THUMB_URL = os.environ.get("THUMB_URL", "https://i.fbcd.co/products/original/ug-circle-logo-design-2-e84695ca2ab9a697d2b2d7c928b0bf5f12bf18e076da241815e0372c8d617915.jpg")
